4baec9a183e283e3619cdf63ec7f20dafcadd5fc
Abi fadila <sipadil28@hotmail.com>
2025-01-12 10:19:45 +0700

    Added Comment service

--- src/main/java/com/fastcampus/blog/controller/CommentController.java
+++ src/main/java/com/fastcampus/blog/controller/CommentController.java
@@ -1,30 +1,31 @@
 package com.fastcampus.blog.controller;
 
 import com.fastcampus.blog.entity.Comment;
+import com.fastcampus.blog.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/comments")
 public class CommentController {
 
+    @Autowired
+    CommentService commentService;
+
     @GetMapping
     public Iterable<Comment> getComments(@RequestParam(required = false) String postSlug,
                                          @RequestParam(required = false) Integer pageNo,
                                          @RequestParam(required = false) Integer limit) {
-        List<Comment> commentList = new ArrayList<>();
-        return commentList;
+       return commentService.getComments(postSlug, pageNo, limit);
     }
 
     @GetMapping("/{id}")
     public Comment getComment(@PathVariable Integer id) {
-        return new Comment();
+        return commentService.getComment(id);
     }
 
     @PostMapping
     public Comment createComment(@RequestBody Comment comment) {
-        return comment;
+        return commentService.createComment(comment);
     }
 }
